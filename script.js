c033be2b731492b80cd2efd0bc727e9cda9ddafe
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-15 14:42:43 +0100

    working with js

--- script.js
+++ script.js
@@ -12,12 +12,28 @@ let allPrices = [
     yearly: 399.99,
   },
 ];
+for (let index = 0; index < allPrices.length; index++) {
+  const element = allPrices[index].monthly;
+  console.log(element);
+}
+for (let index = 0; index < allPrices.length; index++) {
+  const elements = allPrices[index].yearly;
+  console.log(elements);
+}
 
 function togglepriceplans(event) {
   let isChecked = event.currentTarget.checked;
   let toggledClass = document.getElementById("beforeeffect");
+  let toggledClass1 = document.getElementById("beforeeffect1");
+  let toggledClass2 = document.getElementById("beforeeffect2");
+  for (let index = 0; index < allPrices.length; index++) {
+    const element = allPrices[index].monthly;
+    console.log(element);
+  }
+
   if (isChecked) {
     toggledClass.classList.add("aftereffect");
+    toggledClass.innerText = element;
   } else {
     toggledClass.classList.remove("aftereffect");
   }
