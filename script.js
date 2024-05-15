2830a3c101c0dfa86ca7a34cc245ff5b0288fb29
chiomaowuama <108200300+chiomaowuama@users.noreply.github.com>
2024-05-15 14:06:27 +0100

    added array

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+let allPrices = [
+  {
+    monthly: 19.99,
+    yearly: 199.99,
+  },
+  {
+    monthly: 24.99,
+    yearly: 249.99,
+  },
+  {
+    monthly: 39.99,
+    yearly: 399.99,
+  },
+];
+
 function togglepriceplans(event) {
   let isChecked = event.currentTarget.checked;
   let toggledClass = document.getElementById("beforeeffect");
